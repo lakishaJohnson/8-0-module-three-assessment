3ebdd6daa5df888c8837bab98f78b98dfc4bae8e
Lakisha J <lakishajohnson@pursuit.org>
2021-12-16 13:36:06 -0500

    Movies functional

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,13 +1,60 @@
 import React from "react";
 
-function Movies() {
+class Movies extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            movies: [],
+            currentMovie: null
+        }
+    }
+
+    fetchMovies = () => {
+        fetch("https://ghibliapi.herokuapp.com/films")
+        .then((res) => res.json())
+        .then((data) => {
+            // console.log(data, "hello")
+            this.setState({
+                movies: data
+            })
+        })
+    }
+    componentDidMount = () => {
+        this.fetchMovies()
+    }
+
+    handleDropdownChange = (event) => {
+        this.setState({
+            currentMovie: event.target.value
+        })
+    }
+    
+    render() {
+        let dropDownOptions = this.state.movies.map((movie, i) => {
+            return <option key={i} value={movie.title}>{movie.title}</option>
+        })
+        
+        let currentMovieObject = this.state.movies.find((movie) => {
+            return movie.title === this.state.currentMovie
+        })
+
     return (
-      <div className="app">
-        <main>
-          <h1>THIS IS THE MOVIES PAGE</h1>
-        </main>
+      <div className="movies">
+        <h1>Select a Movie</h1>
+        <select onChange={this.handleDropdownChange}>
+            <option>
+                Empty
+            </option>
+           {dropDownOptions} 
+        </select>
+       <h2>
+           {currentMovieObject?.title}
+       </h2>
+       <p>{currentMovieObject?.release_date}</p> <p>{currentMovieObject?.description}</p>
       </div>
     );
   }
+}
   
   export default Movies;
