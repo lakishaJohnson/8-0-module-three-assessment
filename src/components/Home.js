69f225acadd3b188512f44430cec5a0dffabdd6d
Lakisha J <lakishajohnson@pursuit.org>
2021-12-16 13:30:49 -0500

    Home functioning

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,10 @@ class Home extends React.Component {
     render () {
 
     return (
-      <div className="app">
-        <main>
-          <h1>WELCOME To GhibliApp</h1>
-        </main>
+      <div className="home">
+
+          <h1>Welcome to GhibliApp</h1>
+        
       </div>
     );
   }
